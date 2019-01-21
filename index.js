af31f83c90f20b542a5b33632ffb7737dfab4941
Gioacchino Di Nardo <gio.amms@gmail.com>
2019-01-21 08:42:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function logWhisper(string){
     console.log(string.toLowerCase());
   }
 }
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+}
